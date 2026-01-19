e60c3d6ace482eacabf0632e9839c673ff4826ba
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-01-19 16:15:13 +0100

    snack-4 utilizzata funzione map per creare un array con solo i nomi degli autori dei post

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorNames = posts.map((post)=>author = post.author);
+console.log(authorNames);
