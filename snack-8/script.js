4a138024db0091d513d4719823dd390d10b63c1b
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-01-19 16:37:16 +0100

    snack-7 creata una variabile in cui ho inserito l'oggetto all'interno dell'array students con nome "Marco Lanci", creata poi una variabile con all'interno la sua classe

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const studentMarcoLanci= students.find((student) => student.name === "Marco Lanci");
+const classMarcoLanci= studentMarcoLanci.class;
+
+console.log(classMarcoLanci);
