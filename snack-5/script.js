75d0e198522247ec0b0b81bd0f4a4055f9ce243c
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-01-20 18:59:58 +0100

    snack-5 inserita soluzione abbreviata

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,5 +2,11 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
-const evenNums= nums.filter((num)=> {if(num % 2 === 0){return true}});
+
+//!mia soluzione
+// const evenNums= nums.filter((num)=> {if(num % 2 === 0){return true}});
+//     console.log(evenNums);
+
+//! soluzione abbreviata
+const evenNums= nums.filter((num)=> {return num % 2 === 0});
     console.log(evenNums);
