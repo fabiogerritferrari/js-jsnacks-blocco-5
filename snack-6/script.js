9f1639161dd9d2a531041ab4682a659294109486
fabiogerritferrari <fabiogerritferrari@gmail.com>
2026-01-20 19:02:44 +0100

    snack - 6 inserita soluzione abbreviata

--- snack-6/script.js
+++ snack-6/script.js
@@ -13,8 +13,13 @@ const zucchine = [
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
 
-const zucchineLunghe= zucchine.filter((zucchina)=> {if(zucchina.length > 15){return true}});
-const zucchineCorte= zucchine.filter((zucchina)=> {if(zucchina.length < 15){return true}});
+//!mia soluzione
+// const zucchineLunghe= zucchine.filter((zucchina)=> {if(zucchina.length > 15){return true}});
+// const zucchineCorte= zucchine.filter((zucchina)=> {if(zucchina.length < 15){return true}});
+
+//!soluzione abbreviata
+const zucchineCorte= zucchine.filter((zucchina)=> { return zucchina.length > 15});
+const zucchineCorte= zucchine.filter((zucchina)=> { return zucchina.length < 15});
 
 console.log(zucchineLunghe)
 console.log(zucchineCorte)
